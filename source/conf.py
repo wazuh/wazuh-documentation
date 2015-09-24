ca28bbb77048d3e9ca4eb800b79df4c39c8da907
Pedro S <pedro@wazuh.com>
2015-09-24 16:27:43 -0700

    ELK Docs

--- source/conf.py
+++ source/conf.py
@@ -49,9 +49,9 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'WazuhDocs'
+project = u'OSSEC-Wazuh Guide'
 copyright = u'2015, WAZUH, Inc.'
-author = u'Jose Luis Ruiz Ruiz'
+author = u'Wazuh, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -203,7 +203,7 @@ html_static_path = ['nstatic']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'WazuhDocsdoc'
+htmlhelp_basename = 'OSSECWazuhDocs'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -225,8 +225,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  (master_doc, 'WazuhDocs.tex', u'WazuhDocs Documentation',
-   u'Jose Luis Ruiz Ruiz', 'manual'),
+  (master_doc, 'WazuhDocs.tex', u'OSSEC Wazuh Documentation',
+   u'Wazuh, Inc.', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -255,7 +255,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'wazuhdocs', u'WazuhDocs Documentation',
+    (master_doc, 'wazuhdocs', u'OSSEC Wazuh Documentation',
      [author], 1)
 ]
 
@@ -269,8 +269,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  (master_doc, 'WazuhDocs', u'WazuhDocs Documentation',
-   author, 'WazuhDocs', 'One line description of project.',
+  (master_doc, 'WazuhDocs', u'OSSEC Wazuh Documentation',
+   author, 'OSSECWazuh', 'One line description of project.',
    'Miscellaneous'),
 ]
 
