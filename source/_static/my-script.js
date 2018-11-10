c549e08e38b1a6e45b5a6ec2e08bd9db3d853305
Pedro de Castro <pedro@wazuh.com>
2018-11-10 04:23:32 -0800

    Revert version selector

--- source/_static/my-script.js
+++ source/_static/my-script.js
@@ -1,20 +1,14 @@
 /*
- * Wazuh documentation - Version selector script
+ * Wazuh documentation - Version selector script 2
  * Copyright (C) 2018 Wazuh, Inc.
  */
 
 var versions = [
-    {name: "3.7 (current)", url: "/3.7"},
-    {name: "3.6", url: "/3.6"},
-    {name: "3.5", url: "/3.5"},
-    {name: "3.4", url: "/3.4"},
-    {name: "3.3", url: "/3.3"},
-    {name: "3.2", url: "/3.2"},
-    {name: "3.1", url: "/3.1"},
-    {name: "3.0", url: "/3.0"},
+    {name: "3.x (current)", url: "/3.x"},
+    {name: "2.1", url: "/2.1"},
 ];
 
-var current_version = "3.7";
+var current_version = "3.x";
 
 $( document ).ready(function() {
     addVersions();
