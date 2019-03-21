d5ab835d9615954481dc1b5e601b50a19b9dd976
vamera <vamera@outlook.es>
2019-03-21 10:24:03 +0100

    Globaltoc (left sidebar) shows only current subtree (desktop)

--- source/_static/js/style.js
+++ source/_static/js/style.js
@@ -16,6 +16,7 @@ $(function(){
       mainBottom = footerElement.offset().top
 			gTocSpaceBottom = 15,
       gTocSpaceTop = $('#search-lg').height();
+  var breakpoint = 992;
 
 	// Finds current page section in globaltoc
 	$('.globaltoc .toctree-l2.current a').each(function(e){
@@ -28,7 +29,6 @@ $(function(){
 
 	function updateFromHash(){
 		loc = location.hash;
-		console.log(loc);
 		$('.globaltoc .leaf, .globaltoc a.current').removeClass('current');
 		selectLeaf(loc);
 	}
@@ -52,14 +52,13 @@ $(function(){
     });
   }
 
-  /* Page scroll event--------------------------------------------------------------------------------------------------*/
+  /* Page scroll event --------------------------------------------------------------------------------------------------*/
   $('#btn-scroll').on('click', function(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
     return false;
   });
 
   $(window).on('scroll', function(e){
-   //console.log(e.currentTarget);
    var scrollYTopPosition = e.currentTarget.pageYOffset;
 
    // Back to top button
@@ -72,23 +71,40 @@ $(function(){
 
  });
 
-	/* Collapsible globaltoc -------------------------------------------------------------------------------------------*/
-	$('#btn-collapse-globaltoc').on('click', function(){
-		var gtwith = tocWrapperElement.width();
-		tocWrapperElement.addClass('collapsed');
-		tocWrapperElement.animate({'left': (0 - gtwith)+'px'}, function(){
-      //$(this).hide();
-      $('#main-content').addClass('full-width');
+	/* Global toc --------------------------------------------------------------------------------------------------*/
+  function currentToc(){
+    var href;
+    // Gets the name of the first level folder
+    category = document.location.pathname.split('/')[1];
+
+		$('#menu-submenu').find('li').each(function(){
+			console.log(category);
+			console.log($(this).children('a').attr('href'));
+			if ($(this).children('a').attr('href').indexOf(category) >= 0 ) {
+				$(this).addClass('active');
+			} else {
+				$(this).removeClass('active');
+			}
 		});
-	});
 
-  $('#btn-uncollapse-globaltoc').on('click', function(){
-			tocWrapperElement.removeClass('collapsed');
+    if ($(window).width() >= breakpoint ){
+      gTocElement.find('a').each(function(){
+        href = $(this).prop('href');
+				if (href.indexOf(category+'/index.html') > 0 || href.indexOf(category+'/#') > 0){
+					$(this).css('display', 'none');
+				}
+        if ( href.indexOf(category) < 0 ){
+          $(this).closest('.toctree-l1').css('display', 'none');
+        } else {
+          $(this).closest('.toctree-l1').css('display', 'block');
+        }
+      });
+    }
+  }
 
-   tocWrapperElement.animate({'left': 0});
-	 $('#main-content').removeClass('full-width');
-  });
+  currentToc();
 
+	/* Resize event --------------------------------------------------------------------------------------------------*/
 	 $(window).on('resize', function(e){
 		 var curWidth = $(this).width();
      $('table').removeClass('table-responsive');
