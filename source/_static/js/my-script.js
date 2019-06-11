9a0819b79208a04a104b59be9cc2e4ee82732b7e
0ryson <dawei_1990@hotmail.com>
2019-06-11 16:40:07 +0200

    Replaced version selector with a dropdown menu
    The version selector had a default design. Now, it has a custom design
    that fits perfectly with the rest of the page.
    
    In addition, the background color of the sub-options on the menu has
    been changed (#f5f5f5).

--- source/_static/js/my-script.js
+++ source/_static/js/my-script.js
@@ -1,92 +1,56 @@
-/*
- * Wazuh documentation - Version selector script
- * Copyright (C) 2019 Wazuh, Inc.
- */
+jQuery(function($){
 
-var versions = [
-    {name: "3.9 (current)", url: "/new-docu-01"},
-    {name: "3.8", url: "/3.8"},
-    {name: "3.7", url: "/3.7"},
-    {name: "3.6", url: "/3.6"},
-    {name: "3.5", url: "/3.5"},
-    {name: "3.4", url: "/3.4"},
-    {name: "3.3", url: "/3.3"},
-    {name: "3.2", url: "/3.2"},
-    {name: "3.1", url: "/3.1"},
-    {name: "3.0", url: "/3.0"},
-    {name: "2.1", url: "/2.1"},
-];
+    /*
+    * Wazuh documentation - Version selector script
+    * Copyright (C) 2019 Wazuh, Inc.
+    */
 
-var current_version = "3.9";
+    var versions = [
+        {name: "3.9 (current)", url: "/new-docu-01"},
+        {name: "3.8", url: "/3.8"},
+        {name: "3.7", url: "/3.7"},
+        {name: "3.6", url: "/3.6"},
+        {name: "3.5", url: "/3.5"},
+        {name: "3.4", url: "/3.4"},
+        {name: "3.3", url: "/3.3"},
+        {name: "3.2", url: "/3.2"},
+        {name: "3.1", url: "/3.1"},
+        {name: "3.0", url: "/3.0"},
+        {name: "2.1", url: "/2.1"},
+    ];
 
-$( document ).ready(function() {
+    var current_version = "new-docu-01";
+
     addVersions();
-    checkLatestDocs();
-});
 
-function addVersions() {
-    var version = $(".version");
-    var select_version = $("#select-version");
-    var path = document.location.pathname.split('/')[1];
+    function addVersions() {
+        var version = $(".version");
+        var select_version = $("#select-version");
+        var select_version_current = $("#select-version .current");
+        var select_version_ul = $("#select-version .dropdown-menu");
+        var path = document.location.pathname.split('/')[1];
 
-    if (version == null) {
-        console.error("No such element of class 'version'");
-        return;
-    }
-
-    if (select_version == null) {
-        console.error("No such element 'select-version'");
-        return;
-    }
-
-    if (path == "current" || path == "3.x" || path == "new-docu-01") {
-        path = current_version;
-    }
-
-    for (var i = 0; i < versions.length; i++) {
-        option = document.createElement("option");
-        option.text = versions[i].name;
-        option.value = versions[i].url;
-        select_version.append(option);
-    }
-
-    select_version.val('/' + path);
-
-    select_version.change(function(event) {
-        var pathTokens = document.location.pathname.split('/');
-        var extraPath = '';
-        if( pathTokens.length >= 3 ) {
-            for(var i = 2; i < pathTokens.length; i++) {
-                extraPath += '/' + pathTokens[i];
-            }
+        if (version == null) {
+            console.error("No such element of class 'version'");
+            return;
         }
 
-        if(extraPath === '/not_found.html') extraPath = '';
+        if (select_version == null) {
+            console.error("No such element 'select-version'");
+            return;
+        }
 
-        $.ajax({
-            type: 'HEAD',
-            url: event.target.value + extraPath,
-            success: function(){
-                window.location.href = event.target.value + extraPath;
-            },
-            error: function() {
-                window.location.href = event.target.value + '/not_found.html';
-            }
-        });
-    });
-}
+        if (path == "current" || path == "3.x" || path == "new-docu-01") {
+            path = current_version;
+        }
 
-function checkLatestDocs(){
-  /* Shows a warning message to the user if current doc version is not the latest version */
-  /* Note: For this to work, it requires the documentation version variable (in file conf.py) and the array of versions (in this script) to be updated */
-  var currentVersion = document.querySelector('.no-latest-notice').getAttribute('data-version');
-  var latestVersion = versions[0].url.replace('/','');
-  if ( currentVersion !== latestVersion ){
-    var page = document.querySelector('#page');
-    page.classList.add('no-latest-docs');
-  }
+        for (var i = 0; i < versions.length; i++) {
+            var ele = "<li><a href='"+versions[i].url+"'>"+versions[i].name+"</a></li>";
+            select_version_ul.append(ele);
+        }
 
-  // Updates link to the latest version with the correct path (documentation's home)
-  var link = document.querySelector('.link-latest');
-  link.setAttribute('href', 'https://' + window.location.hostname + '/' + latestVersion + '/index.html');
-}
+        select_version_current.html(versions[0].name);
+
+    }
+
+});
