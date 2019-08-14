a50d7249f8e2b008814acbf4fa82b9a1f67eb45f
vamera <vanesa.mejias@wazuh.com>
2019-08-14 14:19:07 +0200

    Removed substring '-new-design'

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ author = u'Wazuh, Inc.'
 copyright = u'&copy; ' + str(datetime.datetime.now().year) + u' &middot; Wazuh Inc.'
 
 # The short X.Y version
-version = '3.0-new-design'
+version = '2.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
