55cf4ac6a7d49bc26fdf1e64b754a5bcd03a2e78
0ryson <david.merino@wazuh.com>
2019-08-21 12:38:41 +0200

    Version on the conf.py changed to 3.1

--- source/conf.py
+++ source/conf.py
@@ -29,7 +29,7 @@ author = u'Wazuh, Inc.'
 copyright = u'&copy; ' + str(datetime.datetime.now().year) + u' &middot; Wazuh Inc.'
 
 # The short X.Y version
-version = '2.1'
+version = '3.1'
 # The full version, including alpha/beta/rc tags
 release = version
 
