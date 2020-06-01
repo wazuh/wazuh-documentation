5fcfeae9c687ed4302aa38fa9c674878544b7bd2
vamera <vanesa.mejias@wazuh.com>
2020-06-01 13:24:14 +0200

    Fixed errors in search results when compiling with Sphinx 3

--- source/conf.py
+++ source/conf.py
@@ -37,7 +37,7 @@ release = version
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -45,7 +45,7 @@ release = version
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.images',
-    'sphinxprettysearchresults',
+    'sphinx_tabs.tabs',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -418,7 +418,7 @@ custom_replacements = {
     "|ELASTICSEARCH_LATEST_DOCKER|" : "7.6.1",
     "|SPLUNK_LATEST|" : "8.0.1",
     "|ELASTIC_6_LATEST|" : "6.8.7",
-}   
+}
 
 # -- Setup -------------------------------------------------------------------
 
