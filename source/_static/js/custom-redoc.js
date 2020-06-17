483a345eb4be6052e2b542190b0b21078c2a219b
vamera <vanesa.mejias@wazuh.com>
2020-06-17 14:34:22 +0200

    Changed the style of the button that collapses the responsive menu in the API reference page

--- source/_static/js/custom-redoc.js
+++ source/_static/js/custom-redoc.js
@@ -12,7 +12,26 @@ function init() {
       && mutationsList[i].addedNodes[0]
       && mutationsList[i].addedNodes[0].classList.contains('redoc-wrap')) {
         // When redoc is finally loaded
+        // Show custom header
         document.querySelector('body').classList.remove('loading');
+
+        // Change toggle-menu button
+        const removed = document.querySelector('.sc-cLQEGU.bPGAgL');
+        const btn = removed.parentNode;
+        const icon = document.createElement('i');
+        icon.classList.add('x');
+        btn.removeChild(removed);
+        btn.classList.add('btn-close');
+        btn.classList.add('collapsed');
+        btn.appendChild(icon);
+
+        btn.addEventListener('click', function() {
+          if (this.classList.contains('collapsed')) {
+            this.classList.remove('collapsed');
+          } else {
+            this.classList.add('collapsed');
+          }
+        });
       }
     }
   };
