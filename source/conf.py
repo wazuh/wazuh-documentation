40b0a7872018b45067be4f6da8ec35edbbb69453
vamera <vanesa.mejias@wazuh.com>
2020-07-06 09:00:40 +0200

    Fixed errors in search results when compiling with Sphinx 3

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,7 @@ release = version
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -46,7 +46,6 @@ release = version
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.images',
-    'sphinxprettysearchresults',
     'sphinx_tabs.tabs',
 ]
 
