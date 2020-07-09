137cde76ecc33f28d80e42db076414ca8fff1b27
David <hola@davidfmiranda.com>
2020-07-09 12:29:34 +0200

    Splunk version bump

--- source/conf.py
+++ source/conf.py
@@ -418,7 +418,7 @@ custom_replacements = {
     "|ELASTICSEARCH_LATEST_KUBERNETES|" : "7.6.2",
     "|ELASTICSEARCH_LATEST_PUPPET|" : "7.6.2",
     "|ELASTICSEARCH_LATEST_DOCKER|" : "7.6.2",
-    "|SPLUNK_LATEST|" : "8.0.2",
+    "|SPLUNK_LATEST|" : "8.0.4",
     "|ELASTIC_6_LATEST|" : "6.8.8",
     "|WAZUH_REVISION_AIX|" : "1",
     "|WAZUH_REVISION_YUM_AGENT_I386|" : "1",
