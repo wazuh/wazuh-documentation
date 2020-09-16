8c94c0e7e694c3c167764b1721c10eaf77f68790
David <hola@davidfmiranda.com>
2020-09-16 12:33:35 +0200

    Changed the Search Guard's location to home

--- resources/open-distro/unattended-installation/all-in-one-installation.sh
+++ resources/open-distro/unattended-installation/all-in-one-installation.sh
@@ -188,18 +188,18 @@ installElasticsearch() {
         eval "rm /etc/elasticsearch/esnode-key.pem /etc/elasticsearch/esnode.pem /etc/elasticsearch/kirk-key.pem /etc/elasticsearch/kirk.pem /etc/elasticsearch/root-ca.pem -f ${debug}"
         eval "mkdir /etc/elasticsearch/certs ${debug}"
         eval "cd /etc/elasticsearch/certs ${debug}"
-        eval "curl -so /etc/elasticsearch/certs/search-guard-tlstool-1.8.zip https://maven.search-guard.com/search-guard-tlstool/1.8/search-guard-tlstool-1.8.zip --max-time 300 ${debug}"
-        eval "unzip search-guard-tlstool-1.8.zip -d searchguard ${debug}"
-        eval "curl -so /etc/elasticsearch/certs/searchguard/search-guard.yml https://raw.githubusercontent.com/wazuh/wazuh-documentation/2205-Open_Distro_installation/resources/open-distro/searchguard/search-guard-aio.yml --max-time 300 ${debug}"
-        eval "chmod +x searchguard/tools/sgtlstool.sh ${debug}"
-        eval "./searchguard/tools/sgtlstool.sh -c ./searchguard/search-guard.yml -ca -crt -t /etc/elasticsearch/certs/ ${debug}"
+        eval "curl -so ~/search-guard-tlstool-1.8.zip https://maven.search-guard.com/search-guard-tlstool/1.8/search-guard-tlstool-1.8.zip --max-time 300 ${debug}"
+        eval "unzip ~/search-guard-tlstool-1.8.zip -d ~/searchguard ${debug}"
+        eval "curl -so ~/searchguard/search-guard.yml https://raw.githubusercontent.com/wazuh/wazuh-documentation/2205-Open_Distro_installation/resources/open-distro/searchguard/search-guard-aio.yml --max-time 300 ${debug}"
+        eval "chmod +x ~/searchguard/tools/sgtlstool.sh ${debug}"
+        eval "bash ~/searchguard/tools/sgtlstool.sh -c ~/searchguard/search-guard.yml -ca -crt -t /etc/elasticsearch/certs/ ${debug}"
         if [  "$?" != 0  ]; then
             echo "Error: certificates were not created"
             exit 1;
         else
             logger "Certificates created"
         fi     
-        eval "rm /etc/elasticsearch/certs/client-certificates.readme /etc/elasticsearch/certs/elasticsearch_elasticsearch_config_snippet.yml search-guard-tlstool-1.8.zip -f ${debug}"
+        eval "rm /etc/elasticsearch/certs/client-certificates.readme /etc/elasticsearch/certs/elasticsearch_elasticsearch_config_snippet.yml ~/search-guard-tlstool-1.8.zip -f ${debug}"
         
         # Configure JVM options for Elasticsearch
         ram_gb=$(free -g | awk '/^Mem:/{print $2}')
