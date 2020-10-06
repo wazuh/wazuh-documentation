ede3c4c893c804a98605ded49b6258ec34909742
David <hola@davidfmiranda.com>
2020-10-06 18:45:56 +0200

    Version bump

--- resources/elastic-stack/unattended-installation/all-in-one-installation.sh
+++ resources/elastic-stack/unattended-installation/all-in-one-installation.sh
@@ -209,13 +209,13 @@ installElasticsearch() {
 
     if [ $sys_type == "yum" ] 
     then
-        eval "yum install elasticsearch-7.8.1 -y -q $debug"
+        eval "yum install elasticsearch-7.9.1 -y -q $debug"
     elif [ $sys_type == "apt-get" ] 
     then
-        eval "apt-get install elasticsearch=7.8.1 -y -q $debug"
+        eval "apt-get install elasticsearch=7.9.1 -y -q $debug"
     elif [ $sys_type == "zypper" ] 
     then
-        eval "zypper -n install elasticsearch-7.8.1 $debug"
+        eval "zypper -n install elasticsearch-7.9.1 $debug"
     fi
 
     if [  "$?" != 0  ]
@@ -281,13 +281,13 @@ installFilebeat() {
     logger "Installing Filebeat..."
     if [ $sys_type == "yum" ] 
     then
-        eval "yum install filebeat-7.8.1 -y -q  $debug"    
+        eval "yum install filebeat-7.9.1 -y -q  $debug"    
     elif [ $sys_type == "zypper" ] 
     then
-        eval "zypper -n install filebeat-7.8.1 $debug"
+        eval "zypper -n install filebeat-7.9.1 $debug"
     elif [ $sys_type == "apt-get" ] 
     then
-        eval "apt-get install filebeat=7.8.1 -y -q  $debug"
+        eval "apt-get install filebeat=7.9.1 -y -q  $debug"
     fi
     if [  "$?" != 0  ]
     then
@@ -318,13 +318,13 @@ installKibana() {
     logger "Installing Kibana..."
     if [ $sys_type == "yum" ] 
     then
-        eval "yum install kibana-7.8.1 -y -q  $debug"    
+        eval "yum install kibana-7.9.1 -y -q  $debug"    
     elif [ $sys_type == "zypper" ] 
     then
-        eval "zypper -n install kibana-7.8.1 $debug"
+        eval "zypper -n install kibana-7.9.1 $debug"
     elif [ $sys_type == "apt-get" ] 
     then
-        eval "apt-get install kibana=7.8.1 -y -q  $debug"
+        eval "apt-get install kibana=7.9.1 -y -q  $debug"
     fi
     if [  "$?" != 0  ]
     then
@@ -335,7 +335,7 @@ installKibana() {
         eval "cd /usr/share/kibana $debug"
         eval "chown -R kibana:kibana /usr/share/kibana/optimize $debug"
         eval "chown -R kibana:kibana /usr/share/kibana/plugins $debug"
-        eval "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages-dev.wazuh.com/pre-release/ui/kibana/wazuh_kibana-4.0.0_7.8.1-1.zip $debug"
+        eval "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages-dev.wazuh.com/pre-release/ui/kibana/wazuh_kibana-4.0.0_7.9.1-1.zip $debug"
         if [  "$?" != 0  ]
         then
             echo "Error: Wazuh Kibana plugin could not be installed."
