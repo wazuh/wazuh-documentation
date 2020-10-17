1c611a8e78fb833650855f21997c9dfbeb515bb0
David <hola@davidfmiranda.com>
2020-10-17 12:43:52 +0200

    Added Copyright

--- resources/elastic-stack/unattended-installation/all-in-one-installation.sh
+++ resources/elastic-stack/unattended-installation/all-in-one-installation.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Program to build the Wazuh Virtual Machine
+# Wazuh package generator
+# Copyright (C) 2015-2020, Wazuh Inc.
+#
+# This program is a free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation.
+
 ## Check if system is based on yum or apt-get or zypper
 char="#"
 debug='> /dev/null 2>&1'
