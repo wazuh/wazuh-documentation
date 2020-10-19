2c51351a63cb72bf02950b102400f3ba8b3a979b
David <hola@davidfmiranda.com>
2020-10-19 12:30:30 +0200

    Changed YUM repo to 4.x

--- resources/elastic-stack/unattended-installation/distributed/wazuh-server-installation.sh
+++ resources/elastic-stack/unattended-installation/distributed/wazuh-server-installation.sh
@@ -192,7 +192,7 @@ addWazuhrepo() {
 		gpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH
 		enabled=1
 		name=Wazuh repository
-		baseurl=https://packages.wazuh.com/3.x/yum/
+		baseurl=https://packages.wazuh.com/4.x/yum/
 		protect=1
 		EOF
     
