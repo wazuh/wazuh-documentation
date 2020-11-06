ac36d370b20d95864fd80d4688eea14d7a2e674f
David <hola@davidfmiranda.com>
2020-11-06 16:17:33 +0100

    Added root check

--- resources/elastic-stack/unattended-installation/all-in-one-installation.sh
+++ resources/elastic-stack/unattended-installation/all-in-one-installation.sh
@@ -459,7 +459,12 @@ main() {
             *)
                 getHelp
             esac
-        done    
+        done 
+
+        if [ "$EUID" -ne 0 ]; then
+            echo "This script must be run as root."
+            exit 1;
+        fi           
 
         if [ -n "$d" ]
         then
--- resources/open-distro/unattended-installation/distributed/wazuh-server-installation.sh
+++ resources/open-distro/unattended-installation/distributed/wazuh-server-installation.sh
@@ -271,6 +271,12 @@ main() {
                 getHelp
             esac
         done
+
+        if [ "$EUID" -ne 0 ]; then
+            echo "This script must be run as root."
+            exit 1;
+        fi
+                
         if [ -n "${debugEnabled}" ]
         then
             debug=""
