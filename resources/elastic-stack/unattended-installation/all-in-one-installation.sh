5bc3afc7c432e5ea8bdab1a76b52f892df72a49f
David <hola@davidfmiranda.com>
2020-12-02 11:09:01 +0100

    Fix typo

--- resources/elastic-stack/unattended-installation/all-in-one-installation.sh
+++ resources/elastic-stack/unattended-installation/all-in-one-installation.sh
@@ -380,7 +380,7 @@ healthCheck() {
         echo "Your system does not meet the recommended minimum hardware requirements of 4Gb of RAM and 2 CPU cores . If you want to proceed with the installation use the -i option to ignore these requirements."
         exit 1;
     elif [[ -f /etc/elasticsearch/elasticsearch.yml ]] && [[ -f /etc/kibana/kibana.yml ]] && [[ -f /etc/filebeat/filebeat.yml ]]; then
-        echo "All the componets have already been installed."
+        echo "All the componens have already been installed."
         exit 1;    
     else
         echo "Starting the installation..."
