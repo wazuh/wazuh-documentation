e03949f100c2c170d578f0c444b9c3ed1c4fc423
Paula Licausi <paulicausi@gmail.com>
2020-12-29 15:00:31 -0300

    Check if path has more than one redirect

--- source/_static/js/canonical-urls.js
+++ source/_static/js/canonical-urls.js
@@ -4,6 +4,7 @@ function addCanonicalUrls() {
     var path = location.pathname;
     var host = location.host;
     var isRemoved = false;
+    var removedRelease;
 
     var releases = [
         '3.13', 
@@ -23,45 +24,63 @@ function addCanonicalUrls() {
         '2.1'
     ];
 
-    releases.forEach(release => {
+    /**
+     * Check if the path has redirects
+     * Change the path until it has no more redirects
+    **/
+    var k = 0;
 
-        /* Check if current path is removed */
-        if(removedUrls[release] && removedUrls[release].includes(path)) {      
-            isRemoved = true;
-        }
-
-    });
-    
-    /* Check if path has redirection */
-    for (let i = 0; i < redirections.length; i++) {
-        let redirection = Object.values(redirections[i]);
-        let release = Object.keys(redirections[i]);
-        
-        for (let j = 0; j < redirection.length; j++) {
-            if (path === redirection[1]) {
-                path = redirection[2];
-                host = host.replace(release[1], release[2]);
+    do {
+        k++;
+        for (let i = 0; i < redirections.length; i++) {
+            let redirection = Object.values(redirections[i]);
+            let release = Object.keys(redirections[i]);
+            
+            for (let j = 0; j < redirection.length; j++) {
+                if (path === redirection[1]) {
+                    path = redirection[2];
+                    host = host.replace(release[1], release[2]);
+                }
             }
         }
 
-    }
+    } while (k < redirections.length);
 
+    /**
+     * Check if the path is removed (in case it isn't, we assume it is available in release 4.0) 
+    **/
+    releases.forEach(release => {
+
+        if(removedUrls[release] && removedUrls[release].includes(path)) {      
+            isRemoved = true;
+            if(isRemoved) {
+                release = release - 0.1;
+                removedRelease = release;
+            }
+        }
+
+    });
+
+    /**
+     * If the path wasn't removed, replace the release number with 4.0
+     * If the path was removed, replace the release number with the number of the latest release it was available 
+    **/
     for (var i = 0; i < releases.length; i++) {
 
-        /* Replace with 4.0 release number in path if isn't removed */
         if (host.indexOf(releases[i]) > -1) {
-            host = isRemoved ? host : host.replace(releases[i], '4.0');
+            host = isRemoved ? host.replace(releases[i], removedRelease) : host.replace(releases[i], '4.0');
         }
 
     }
 
-    /* Link rel=canonical tag creation */
+    /**
+     * Link rel=canonical tag creation
+    **/
     var canonicalTag = !!document.querySelector("link[rel='canonical']") ? document.querySelector("link[rel='canonical']") : document.createElement('link');
     canonicalTag.setAttribute('rel', 'canonical');
     
     canonicalTag.setAttribute('href', location.protocol + '//' + host + path);
 
-    /* Append to head */
     document.head.appendChild(canonicalTag);
 
 }
