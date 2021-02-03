c154716f8cd1ec45f6fb9ad8b4b09d81d2d63857
David <hola@davidfmiranda.com>
2021-02-03 17:18:27 +0100

    Version bump

--- resources/elastic-stack/unattended-installation/distributed/elastic-stack-installation.sh
+++ resources/elastic-stack/unattended-installation/distributed/elastic-stack-installation.sh
@@ -214,13 +214,13 @@ installElasticsearch() {
 
     if [ $sys_type == "yum" ]
     then
-        eval "yum install elasticsearch-7.9.3 -y -q $debug"
+        eval "yum install elasticsearch-7.10.0 -y -q $debug"
     elif [ $sys_type == "apt-get" ] 
     then
-        eval "apt-get install elasticsearch=7.9.3 -y -q $debug"
+        eval "apt-get install elasticsearch=7.10.0 -y -q $debug"
     elif [ $sys_type == "zypper" ] 
     then
-        eval "zypper -n install elasticsearch-7.9.3 $debug"
+        eval "zypper -n install elasticsearch-7.10.0 $debug"
     fi
 
     if [  "$?" != 0  ]
@@ -418,13 +418,13 @@ installKibana() {
     logger "Installing Kibana..."
     if [ $sys_type == "yum" ]
     then
-        eval "yum install kibana-7.9.3 -y -q  $debug"    
+        eval "yum install kibana-7.10.0 -y -q  $debug"    
     elif [ $sys_type == "zypper" ] 
     then
-        eval "zypper -n install kibana-7.9.3 $debug"
+        eval "zypper -n install kibana-7.10.0 $debug"
     elif [ $sys_type == "apt-get" ] 
         then
-        eval "apt-get install kibana=7.9.3 -y -q  $debug"
+        eval "apt-get install kibana=7.10.0 -y -q  $debug"
     fi
     if [  "$?" != 0  ]
     then
@@ -433,12 +433,11 @@ installKibana() {
     else
         disableRepos
         eval "curl -so /etc/kibana/kibana.yml https://raw.githubusercontent.com/wazuh/wazuh-documentation/4.0/resources/elastic-stack/unattended-installation/distributed/templates/kibana_unattended.yml --max-time 300 $debug"
-        eval "cd /usr/share/kibana $debug"
-        eval "chown -R kibana:kibana /usr/share/kibana/optimize $debug"
-        eval "chown -R kibana:kibana /usr/share/kibana/plugins $debug"        
-        eval "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.1.0_7.9.3-1.zip $debug"
-        if [  "$?" != 0  ]
-        then
+        eval "mkdir /usr/share/kibana/data ${debug}"
+        eval "chown -R kibana:kibana /usr/share/kibana/data ${debug}"
+        eval "cd /usr/share/kibana ${debug}"
+        eval "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/4.x/ui/kibana/wazuh_kibana-4.1.0_7.10.0-1.zip ${debug}"
+        if [  "$?" != 0  ]; then
             echo "Error: Wazuh Kibana plugin could not be installed."
             exit 1;
         fi
