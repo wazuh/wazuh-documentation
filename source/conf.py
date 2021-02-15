29c5293c45c04b11abb915a516ec66d637064e5b
David <hola@davidfmiranda.com>
2021-02-15 18:33:53 +0100

    Fixed api_tag redoc

--- source/conf.py
+++ source/conf.py
@@ -33,8 +33,9 @@ is_latest_release = True
 
 # The full version, including alpha/beta/rc tags
 # Important: use a valid branch (4.0) or, preferably, tag name (v4.0.0)
-release = 'v4.1.0'
-apiURL = 'https://raw.githubusercontent.com/wazuh/wazuh/'+release+'/api/api/spec/spec.yaml'
+release = '4.1'
+api_tag = 'v4.1.0'
+apiURL = 'https://raw.githubusercontent.com/wazuh/wazuh/'+api_tag+'/api/api/spec/spec.yaml'
 
 # -- General configuration ------------------------------------------------
 
