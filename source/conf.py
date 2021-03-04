da043aadf4acc49adae951f9eae044c2a36190d0
David <hola@davidfmiranda.com>
2021-03-04 09:20:54 +0100

    Wazuh version bump

--- source/conf.py
+++ source/conf.py
@@ -432,8 +432,8 @@ def customReplacements(app, docname, source):
 
 custom_replacements = {
     "|CURRENT_MAJOR|" : "4.x",
-    "|WAZUH_LATEST|" : "4.1.1",
-    "|WAZUH_LATEST_MINOR|": "4.1",
+    "|WAZUH_LATEST|" : "4.2.0",
+    "|WAZUH_LATEST_MINOR|": "4.2",
     "|WAZUH_LATEST_ANSIBLE|" : "4.0.4",
     "|WAZUH_LATEST_KUBERNETES|" : "4.1.0",
     "|WAZUH_LATEST_PUPPET|" : "4.0.4",
