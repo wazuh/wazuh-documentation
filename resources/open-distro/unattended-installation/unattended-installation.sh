6ad2abf396ab8f73ba8d966b02b270bffee3fee8
David <hola@davidfmiranda.com>
2021-03-16 13:20:16 +0100

    Removed apt versions

--- resources/open-distro/unattended-installation/unattended-installation.sh
+++ resources/open-distro/unattended-installation/unattended-installation.sh
@@ -326,7 +326,7 @@ installElasticsearch() {
     elif [ ${sys_type} == "zypper" ]; then
         eval "zypper -n install opendistroforelasticsearch=${OD_VER}-${OD_REV} ${debug}"
     elif [ ${sys_type} == "apt-get" ]; then
-        eval "apt install elasticsearch-oss=7.10.0 opendistro-alerting=1.11.0.1-1 opendistro-anomaly-detection=1.11.0.0-1 opendistro-index-management=1.11.0.0-1 opendistro-job-scheduler=1.11.0.0-1 opendistro-knn=1.11.0.0-1 opendistro-knnlib=1.11.0.0 opendistro-performance-analyzer=1.11.0.0-1 opendistro-security=1.11.0.0-0 opendistro-sql=1.11.0.0-1 opendistroforelasticsearch=1.11.0-1 -y ${debug}"
+        eval "apt install elasticsearch-oss opendistroforelasticsearch -y ${debug}"
     fi
 
     if [  "$?" != 0  ]; then
