9283dcbb93964ab434f5b4e15862ad50e8ecc9cd
Santiago Bruno <santiago.bruno@wazuh.com>
2021-03-26 10:43:19 -0300

    Updating wazuh-passwords-tool for wazuh-indexer

--- resources/open-distro/tools/wazuh-passwords-tool.sh
+++ resources/open-distro/tools/wazuh-passwords-tool.sh
@@ -17,6 +17,8 @@ elif [ -n "$(command -v apt-get)" ]; then
     SYS_TYPE="apt-get"   
 fi
 
+SERVICE_NAME='elasticsearch'
+
 ## Checks if the script is run with enough privileges
 
 checkRoot() {
@@ -74,7 +76,7 @@ getHelp() {
 ## Gets the network host
 
 getNetworkHost() {
-    IP=$(grep -hr "network.host:" /etc/elasticsearch/elasticsearch.yml)
+    IP=$(grep -hr "network.host:" /etc/${SERVICE_NAME}/elasticsearch.yml)
     NH="network.host: "
     IP="${IP//$NH}"
 }
@@ -84,16 +86,32 @@ getNetworkHost() {
 checkInstalled() {
     
     if [ "${SYS_TYPE}" == "yum" ]; then
-        elasticinstalled=$(yum list installed 2>/dev/null | grep opendistroforelasticsearch)
+        elasticinstalled=$(yum list installed 2>/dev/null | grep wazuh-indexer)
     elif [ "${SYS_TYPE}" == "zypper" ]; then
-        elasticinstalled=$(zypper packages --installed | grep opendistroforelasticsearch | grep i+ | grep noarch)
+        elasticinstalled=$(zypper packages --installed | grep wazuh-indexer)
     elif [ "${SYS_TYPE}" == "apt-get" ]; then
-        elasticinstalled=$(apt list --installed  2>/dev/null | grep opendistroforelasticsearch)
+        elasticinstalled=$(apt list --installed  2>/dev/null | grep wazuh-indexer)
     fi 
 
     if [ -z "${elasticinstalled}" ]; then
-        echo "Error: Open Distro is not installed on the system."
+        if [ "${SYS_TYPE}" == "yum" ]; then
+            elasticinstalled=$(yum list installed 2>/dev/null | grep opendistroforelasticsearch | grep -v opendistroforelasticsearch-kibana)
+        elif [ "${SYS_TYPE}" == "zypper" ]; then
+            elasticinstalled=$(zypper packages --installed | grep opendistroforelasticsearch | grep i+ | grep noarch | grep -v opendistroforelasticsearch-kibana)
+        elif [ "${SYS_TYPE}" == "apt-get" ]; then
+            elasticinstalled=$(apt list --installed  2>/dev/null | grep opendistroforelasticsearch | grep -v opendistroforelasticsearch-kibana)
+        fi
+    else
+        SERVICE_NAME="wazuh-indexer"
+        echo "wazuh-indexer package is installed in the system"
+    fi
+
+    echo "${elasticinstalled}"
+    if [ -z "${elasticinstalled}" ]; then
+        echo "Error: Neither Open Distro nor wazuh-indexer are installed on the system."
         exit 1;
+    else
+        echo "wazuh-indexer package is installed in the system"
     fi
 
 }
@@ -101,7 +119,7 @@ checkInstalled() {
 ## Reads all the users present in internal_users.yml
 
 readUsers() {
-    SUSERS=$(grep -B 1 hash: /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/internal_users.yml | grep -v hash: | grep -v "-" | awk '{ print substr( $0, 1, length($0)-1 ) }')
+    SUSERS=$(grep -B 1 hash: /usr/share/${SERVICE_NAME}/plugins/opendistro_security/securityconfig/internal_users.yml | grep -v hash: | grep -v "-" | awk '{ print substr( $0, 1, length($0)-1 ) }')
     USERS=($SUSERS)  
 }
 
@@ -123,11 +141,10 @@ checkUser() {
 ## Creates a backup of the existing insternal_users.yml
 
 createBackUp() {
-    
     echo "Creating backup..."
-    eval "mkdir /usr/share/elasticsearch/backup ${VERBOSE}"
-    eval "cd /usr/share/elasticsearch/plugins/opendistro_security/tools/ ${VERBOSE}"
-    eval "./securityadmin.sh -backup /usr/share/elasticsearch/backup -nhnv -cacert /etc/elasticsearch/certs/root-ca.pem -cert /etc/elasticsearch/certs/admin.pem -key /etc/elasticsearch/certs/admin-key.pem -icl -h ${IP} ${VERBOSE}"
+    eval "mkdir /usr/share/${SERVICE_NAME}/backup ${VERBOSE}"
+    eval "cd /usr/share/${SERVICE_NAME}/plugins/opendistro_security/tools/ ${VERBOSE}"
+    eval "./securityadmin.sh -backup /usr/share/${SERVICE_NAME}/backup -nhnv -cacert /etc/${SERVICE_NAME}/certs/root-ca.pem -cert /etc/${SERVICE_NAME}/certs/admin.pem -key /etc/${SERVICE_NAME}/certs/admin-key.pem -icl -h ${IP} ${VERBOSE}"
     if [  "$?" != 0  ]; then
         echo "Error: The backup could not be created"
         exit 1;
@@ -167,13 +184,13 @@ generateHash() {
         echo "Generating hashes"
         for i in "${!PASSWORDS[@]}"
         do
-            NHASH=$(bash /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p ${PASSWORDS[i]} | grep -v WARNING)
+            NHASH=$(bash /usr/share/${SERVICE_NAME}/plugins/opendistro_security/tools/hash.sh -p ${PASSWORDS[i]} | grep -v WARNING)
             HASHES+=(${NHASH})
         done
         echo "Hashes generated"
     else
         echo "Generating hash"
-        HASH=$(bash /usr/share/elasticsearch/plugins/opendistro_security/tools/hash.sh -p ${PASSWORD} | grep -v WARNING)
+        HASH=$(bash /usr/share/${SERVICE_NAME}/plugins/opendistro_security/tools/hash.sh -p ${PASSWORD} | grep -v WARNING)
         if [  "$?" != 0  ]; then
             echo "Error: Hash generation failed."
             exit 1;
@@ -190,7 +207,7 @@ changePassword() {
     if [ -n "${CHANGEALL}" ]; then
         for i in "${!PASSWORDS[@]}"
         do
-           awk -v new=${HASHES[i]} 'prev=="'${USERS[i]}':"{sub(/\042.*/,""); $0=$0 new} {prev=$1} 1' /usr/share/elasticsearch/backup/internal_users.yml > internal_users.yml_tmp && mv -f internal_users.yml_tmp /usr/share/elasticsearch/backup/internal_users.yml
+           awk -v new=${HASHES[i]} 'prev=="'${USERS[i]}':"{sub(/\042.*/,""); $0=$0 new} {prev=$1} 1' /usr/share/${SERVICE_NAME}/backup/internal_users.yml > internal_users.yml_tmp && mv -f internal_users.yml_tmp /usr/share/${SERVICE_NAME}/backup/internal_users.yml
 
             if [ "${USERS[i]}" == "admin" ]; then
                 adminpass=${PASSWORDS[i]}
@@ -200,7 +217,7 @@ changePassword() {
 
         done
     else
-        awk -v new="$HASH" 'prev=="'${NUSER}':"{sub(/\042.*/,""); $0=$0 new} {prev=$1} 1' /usr/share/elasticsearch/backup/internal_users.yml > internal_users.yml_tmp && mv -f internal_users.yml_tmp /usr/share/elasticsearch/backup/internal_users.yml
+        awk -v new="$HASH" 'prev=="'${NUSER}':"{sub(/\042.*/,""); $0=$0 new} {prev=$1} 1' /usr/share/${SERVICE_NAME}/backup/internal_users.yml > internal_users.yml_tmp && mv -f internal_users.yml_tmp /usr/share/${SERVICE_NAME}/backup/internal_users.yml
 
         if [ "${NUSER}" == "admin" ]; then
             adminpass=${PASSWORD}
@@ -255,14 +272,14 @@ changePassword() {
 runSecurityAdmin() {
     
     echo "Loading changes..."
-    eval "cp /usr/share/elasticsearch/backup/* /usr/share/elasticsearch/plugins/opendistro_security/securityconfig/ ${debug}"
-    eval "cd /usr/share/elasticsearch/plugins/opendistro_security/tools/ ${debug}"
-    eval "./securityadmin.sh -cd ../securityconfig/ -nhnv -cacert /etc/elasticsearch/certs/root-ca.pem -cert /etc/elasticsearch/certs/admin.pem -key /etc/elasticsearch/certs/admin-key.pem ${debug}"
+    eval "cp /usr/share/${SERVICE_NAME}/backup/* /usr/share/${SERVICE_NAME}/plugins/opendistro_security/securityconfig/ ${debug}"
+    eval "cd /usr/share/${SERVICE_NAME}/plugins/opendistro_security/tools/ ${debug}"
+    eval "./securityadmin.sh -cd ../securityconfig/ -nhnv -cacert /etc/${SERVICE_NAME}/certs/root-ca.pem -cert /etc/${SERVICE_NAME}/certs/admin.pem -key /etc/${SERVICE_NAME}/certs/admin-key.pem ${debug}"
     if [  "$?" != 0  ]; then
         echo "Error: Could not load the changes."
         exit 1;
     fi    
-    eval "rm -rf /usr/share/elasticsearch/backup/ ${VERBOSE}"
+    eval "rm -rf /usr/share/${SERVICE_NAME}/backup/ ${VERBOSE}"
     echo "Done"
 
     if [[ -n "${NUSER}" ]] && [[ -n ${AUTOPASS} ]]; then
@@ -320,13 +337,13 @@ main() {
             esac
         done
 
-        export JAVA_HOME=/usr/share/elasticsearch/jdk/
-        
         if [ -n "${VERBOSEENABLED}" ]; then
             VERBOSE=""
         fi 
 
         checkInstalled   
+
+        export JAVA_HOME=/usr/share/${SERVICE_NAME}/jdk/
 
         if [[ -n "${NUSER}" ]] && [[ -n "${CHANGEALL}" ]]; then
             getHelp
