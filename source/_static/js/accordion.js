86ef395aefd059a16d9a4222f308363b9deb8fa6
Paula Licausi <paulicausi@gmail.com>
2021-07-20 11:13:56 -0300

    Fix section accordions headerlinks functionality

--- source/_static/js/accordion.js
+++ source/_static/js/accordion.js
@@ -1,10 +1,11 @@
 $(function() {
 
-    let outputs = $('.output');
+    let outputs = document.getElementsByClassName('output');
 
     for (let i = 0; i < outputs.length; i++) {
-        if(outputs[i].classList.contains('accordion-output')) {
-            let outputTitle = outputs[i].getElementsByClassName('admonition-output')[0];
+        if(outputs[i].classList.contains("accordion-output")) {
+            let outputTitle = outputs[i].getElementsByClassName("admonition-output")[0];
+            let outputBody = outputs[i].getElementsByClassName('highlight')[0];
 
             setAccordionAttributes(outputTitle, {"data-toggle": "collapse",
                                                  "href": "#outputAccordion" + i,
@@ -13,18 +14,21 @@ $(function() {
                                                  "aria-controls": "outputAccordion" + i,
                                                  "class": "collapsed"                                       
                                             });
-                                            
-            $(".highlight", outputs[i]).addClass('collapse')
-                                       .attr("id", "outputAccordion" + i);
+
+            setAccordionAttributes(outputBody, {"id": "outputAccordion" + i } )
+            outputBody.classList.add("collapse");                                        
         }
     }
 
-    let accordions = document.getElementsByClassName('accordion-section');
+    let accordions = document.getElementsByClassName("accordion-section");
 
     for (let j = 0; j < accordions.length; j++) {
         let accordionTitle = accordions[j].previousElementSibling;
         let accordionBody = accordions[j];
-        let sectionId = accordions[j].parentElement.getAttribute('id') + "-accordion";
+        let sectionId = accordions[j].parentElement.getAttribute("id") + "-accordion";
+        let accordionParent = accordions[j].parentElement;
+
+        accordionParent.classList.add("accordion-parent");
 
         setAccordionAttributes(accordionTitle, {"data-toggle": "collapse",
                                                 "href": "#" + sectionId,
@@ -33,10 +37,22 @@ $(function() {
                                                 "aria-controls": sectionId,
                                                 "class": "accordion-title collapsed"                                       
                                             });  
-        $(accordionTitle).prepend('<i class="fas fa-chevron-right"></i>');
 
         setAccordionAttributes(accordionBody, {"class": "accordion-section accordion collapse",
                                                "id": sectionId });
+        
+        accordionTitle.innerHTML = "<i class='fas fa-chevron-right'></i>" + accordionTitle.innerHTML;
+
+        let urlIcon = accordionTitle.getElementsByClassName('headerlink')[0];
+        urlIcon.addEventListener('click', function(e) {
+            if(accordionBody.classList.contains('show')) {
+                e.stopPropagation();
+            }
+        });
+
+        if (window.location.href.indexOf(accordionParent.getAttribute("id")) > -1 && accordionTitle.classList.contains('collapsed')) {
+            accordionTitle.click();
+        }
     }
 
     function setAccordionAttributes(element, attrs) {
@@ -44,6 +60,5 @@ $(function() {
             element.setAttribute(key, attrs[key]);
         }
     }
-    
     
 });
