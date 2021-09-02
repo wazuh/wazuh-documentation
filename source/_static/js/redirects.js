fec3aa1162d09b1f6c1da98a04f9de83f9c604cf
arcapas <dfurfuro@gmail.com>
2021-09-02 08:08:05 -0300

    add_wazuh_rbac

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -65,6 +65,7 @@ newUrls['4.2'] = [
   '/user-manual/reference/statistics-files/wazuh-logcollector-state.html',
   '/user-manual/reference/tools/wazuh-control.html',
   '/user-manual/reference/tools/wazuh-regex.html',
+  '/user-manual/kibana-app/wazuh_rbac.html',
 ];
 
 removedUrls['4.2'] = [
