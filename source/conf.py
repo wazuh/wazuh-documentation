747ab2d4493c489ede8cea3001b4b4d977b9c7ff
s-ocando <sandra.ocando@wazuh.com>
2021-09-21 10:53:32 +0200

    Remove outdated variables

--- source/conf.py
+++ source/conf.py
@@ -446,8 +446,6 @@ def customReplacements(app, docname, source):
 custom_replacements = {
     "|CURRENT_MAJOR|" : "4.x",
     "|WAZUH_LATEST|" : "4.2.2",
-    "|WAZUH_GCC_CHANGE|" : "4.1.4",
-    "|WAZUH_PREGCC_CHANGE|" : "4.1.3",
     "|WAZUH_LATEST_MINOR|" : "4.2",
     "|WAZUH_PACKAGES_BRANCH|" : "4.2",
     "|WAZUH_LATEST_ANSIBLE|" : "4.2.2",
