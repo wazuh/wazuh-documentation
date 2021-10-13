743fd0b3c5e40c21b905bb266aa2d1a58f797d9c
vcerenu <victor.erenu@wazuh.com>
2021-10-13 15:27:57 -0300

    release 4.2.3 Ansible

--- source/conf.py
+++ source/conf.py
@@ -448,7 +448,7 @@ custom_replacements = {
     "|WAZUH_LATEST|" : "4.2.3",
     "|WAZUH_LATEST_MINOR|" : "4.2",
     "|WAZUH_PACKAGES_BRANCH|" : "4.2",
-    "|WAZUH_LATEST_ANSIBLE|" : "4.2.2",
+    "|WAZUH_LATEST_ANSIBLE|" : "4.2.3",
     "|WAZUH_LATEST_KUBERNETES|" : "4.2.2",
     "|WAZUH_LATEST_PUPPET|" : "4.2.2",
     "|WAZUH_LATEST_OVA|" : "4.2.3",
