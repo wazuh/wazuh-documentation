5c78ead0f143e76a2455f0922f29d8e2112dd168
rauldpm <raul.delpozo@wazuh.com>
2021-10-19 17:20:10 +0200

    Fix conflict with docker version

--- source/conf.py
+++ source/conf.py
@@ -453,7 +453,7 @@ custom_replacements = {
     "|WAZUH_LATEST_PUPPET|" : "4.2.2",
     "|WAZUH_LATEST_OVA|" : "4.2.3",
     "|WAZUH_LATEST_AMI|" : "4.2.3",
-    "|WAZUH_LATEST_DOCKER|" : "4.2.2",
+    "|WAZUH_LATEST_DOCKER|" : "4.2.3",
     "|OPEN_DISTRO_LATEST|" : "1.13.2",
     "|ELASTICSEARCH_LATEST|" : "7.10.2",
     "|ELASTICSEARCH_LATEST_OVA|" : "7.10.2",
