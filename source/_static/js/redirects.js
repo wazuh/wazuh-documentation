41bec7b707aa353a709131cb005800c99fad7710
arcapas <dfurfuro@gmail.com>
2021-11-09 07:16:55 -0300

    generate_redirects_4.0_fix

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -395,7 +395,7 @@ newUrls['4.0'] = [
   '/user-manual/uninstall/index.html',
   '/user-manual/uninstall/open-distro.html',
   '/user-manual/certificates.html',
-  '/virtual-machine/virtual-machine.html  '/
+  '/virtual-machine/virtual-machine.html'/
   '/docker/upgrade-guide.html'/
 ];
 
