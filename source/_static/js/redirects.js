9abd80e683b3fe905682bde39370317c77f1772b
Sandra Ocando <sandra.ocando@wazuh.com>
2022-04-20 10:03:13 +0200

    Update redirects

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -353,7 +353,7 @@ redirections.push(
     '4.3': '/installation-guide/index.html',
   },
   {
-    'target': ['4.2=>4.3', '4.3=>4.2'],
+    'target': ['4.2=>4.3'],
     '4.2': '/deployment.html',
     '4.3': '/deployment-options/index.html',
   },
@@ -415,7 +415,7 @@ redirections.push(
   {
     'target': ['4.2=>4.3', '4.3=>4.2'],
     '4.2': '/installation-guide/open-distro/distributed-deployment/unattended/unattended-elasticsearch-cluster-installation.html',
-    '4.3': '/installation-guide/wazuh-indexer/unattended.html',
+    '4.3': '/installation-guide/wazuh-indexer/installation-assistant.html',
   },
   {
     'target': ['4.2=>4.3', '4.3=>4.2'],
@@ -425,7 +425,7 @@ redirections.push(
   {
     'target': ['4.2=>4.3', '4.3=>4.2'],
     '4.2': '/installation-guide/open-distro/distributed-deployment/unattended/unattended-wazuh-cluster-installation.html',
-    '4.3': '/installation-guide/wazuh-server/unattended.html',
+    '4.3': '/installation-guide/wazuh-server/installation-assistant.html',
   },
   {
     'target': ['4.2=>4.3', '4.3=>4.2'],
@@ -433,9 +433,9 @@ redirections.push(
     '4.3': '/installation-guide/wazuh-server/step-by-step.html',
   },
   {
-    'target': ['4.2=>4.3', '4.3=>4.2'],
-    '4.2': 'installation-guide/open-distro/distributed-deployment/unattended/unattended-elasticsearch-cluster-installation.html',
-    '4.3': '/installation-guide/wazuh-dashboard/unattended.html',
+    'target': ['4.3=>4.2'],
+    '4.2': '/installation-guide/open-distro/distributed-deployment/unattended/unattended-elasticsearch-cluster-installation.html',
+    '4.3': '/installation-guide/wazuh-dashboard/installation-assistant.html',
   },
   {
     'target': ['4.2=>4.3', '4.3=>4.2'],
