bc33893adac9a81830cae4abfca9901ee49dc740
vamera <vanesa.mejias@wazuh.com>
2022-05-03 13:18:29 +0200

    Added the new theme

---
+++ source/_themes/wazuh_doc_theme_v3/config/uglifyjs-group.js
@@ -0,0 +1,93 @@
+'use strict'
+
+const path = require('path')
+const fs = require("fs")
+const { nodeResolve } = require('@rollup/plugin-node-resolve')
+const UglifyJS = require("uglify-js");
+const SRC_PATH = "src/js-source/";
+const END_PATH = "static/js/";
+
+const parameter = process.argv[2]; 
+const FILE = parameter.split('=')[1];
+// const cacheFileName = path.resolve(__dirname, "../build/cache.json");
+
+// List of javascript files that must be included in all pages
+const commonJS = [
+  SRC_PATH + "utils.js",
+  SRC_PATH + "components/light-dark-mode.js"
+];
+
+// List of javascript files per page
+const pageSpecificJS = {
+  "wazuh-documentation": [
+    SRC_PATH + "components/in-release-redirections.js",
+    SRC_PATH + "components/admonitions.js",
+    SRC_PATH + "components/accordions.js",
+    SRC_PATH + "components/doctools.js",
+    SRC_PATH + "components/version-selector.js",
+    SRC_PATH + "components/global-toc.js",
+    SRC_PATH + "components/local-toc.js",
+    SRC_PATH + "components/code-blocks.js",
+    SRC_PATH + "content.js"
+    // + JS files for different parts/components of the theme (only functionality for regular documentation content pages) + tabs (extension) + lightbox (extension)
+  ],
+  "index": [
+    SRC_PATH + "components/doctools.js",
+    SRC_PATH + "index.js" //JS code only for page "Index" if necessary.
+  ],
+  "search-results": [
+    SRC_PATH + "components/doctools.js",
+    SRC_PATH + "components/version-selector.js",
+    SRC_PATH + "components/global-toc.js",
+    SRC_PATH + "components/sphinx-search/language_data.js",
+    SRC_PATH + "components/sphinx-search/searchtools.js",
+    SRC_PATH + "search-results.js"
+    // + JS files only for search page
+  ],
+  "not-found": [
+    SRC_PATH + "components/doctools.js",
+    SRC_PATH + "components/version-selector.js",
+    SRC_PATH + "components/global-toc.js"
+    // + JS files only for page "Not found"
+  ],
+  "api-reference": [
+    SRC_PATH + "components/version-selector.js",
+    SRC_PATH + "custom-redoc.js"
+  ]
+};
+
+if (pageSpecificJS.hasOwnProperty(FILE)) {
+  const outputFilename = END_PATH + "min/" + FILE;
+  const options = {
+    "compress": {
+        "typeofs": false
+    },
+    "mangle": true,
+    "sourceMap": {
+      filename: outputFilename + ".min.js",
+      url: FILE + ".min.js.map"
+    }
+  };
+  // nameCache: JSON.parse(fs.readFileSync(cacheFileName, "utf8"))
+  const scriptFiles = commonJS.concat(pageSpecificJS[FILE]);
+  const code = {};
+  for (let i = 0; i < scriptFiles.length; i++) {
+    code[scriptFiles[i]] = fs.readFileSync(path.resolve(__dirname, "../" + scriptFiles[i]), "utf8")
+  }
+  const minified = UglifyJS.minify(code, options);
+
+  if (minified.error) {
+    console.log("Error while minifiying the file \"" + FILE + "\": " + JSON.stringify(minified.error));
+  } else {
+    fs.writeFileSync(outputFilename + ".min.js", minified.code, "utf8");
+    console.log("Created the file "+ outputFilename + ".min.js");
+    if ( minified.map ) {
+      fs.writeFileSync(outputFilename + ".min.js.map", minified.map, "utf8");
+      console.log("Created the file "+ outputFilename + ".min.js.map");
+    } else {
+      console.log("NO MAP!!");
+    }
+  }
+} else {
+  console.log("Error: No list available for \""+ FILE +"\"");
+}
