9b874ad2ddc1162b142a5f19d67ed37ccf3d47fe
arcapas <dfurfuro@gmail.com>
2023-06-01 12:33:15 -0300

    Add new section

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -251,6 +251,7 @@ newUrls['4.4'] = [
   '/user-manual/capabilities/system-calls-monitoring/use-cases/monitoring-commands-run-as-root.html',
   '/user-manual/capabilities/system-calls-monitoring/use-cases/privilege-abuse.html',
   '/user-manual/capabilities/wazuh-archives.html',
+  '/user-manual/capabilities/vulnerability-detection/querying-the-vulnerability-database.html',
 ];
 
 /* Pages removed in 4.4 */
