2f8dc4967833d7d73b1937b3715fc9f5571e32b8
vamera <vanesa.mejias@wazuh.com>
2023-08-31 16:24:02 +0200

    Changed the requirements, updated our current Sphinx theme and removed the old one

---
+++ source/_variables/empty_toc_nodes.py
@@ -0,0 +1,28 @@
+###############################################################################
+#
+# Empty TOC nodes
+#
+# This is a list of paths (without file extension) of documents considered 
+# "empty" since they don't have much content or it is not relevant
+# Usually, the document should not be linked in the left menu.
+#
+
+emptyTocNodes = [
+    'amazon/configuration/index',
+    'containers',
+    'deployment',
+    'docker-monitor/index',
+    'installation-guide/elasticsearch-cluster/index',
+    'installation-guide/wazuh-cluster/index',
+    'installation-guide/upgrading/legacy/index',
+    'installation-guide/packages-list/linux/linux-index',
+    'installation-guide/packages-list/solaris/solaris-index',
+    'user-manual/agents/index',
+    'user-manual/agents/remove-agents/index',
+    'user-manual/agents/listing/index',
+    'user-manual/kibana-app/reference/index',
+    'user-manual/ruleset/ruleset-xml-syntax/index',
+    'installation-guide/distributed-deployment/step-by-step-installation/elasticsearch-cluster/index',
+    'installation-guide/distributed-deployment/step-by-step-installation/wazuh-cluster/index',
+    'user-manual/capabilities/active-response/ar-use-cases/index',
+]
