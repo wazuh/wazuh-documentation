83297a630e3b34fa3af77cc27009e52f8c597ced
vamera <vanesa.mejias@wazuh.com>
2023-08-31 09:17:13 +0200

    Changed the requirements, updated our current Sphinx theme and removed the old one

--- source/_variables/redirect_same_release.py
+++ source/_variables/redirect_same_release.py
@@ -1,6 +1,27 @@
-# -- Redirects within the same release  -------------------------------------------------
-
-# Important: the redirect is relative to the old path
+###############################################################################
+#
+# Redirects within the same release
+#
+# This file contains the dictionary redirects within each release
+# For every release having redirects in the same release there must exist a 
+# key with the release number 'X.Y':
+#
+# redirectSameRelease = {
+#     'X.Y': <value>
+#     ...
+# }
+#
+# Where <value> is a new dictionary where the keys represent the old path
+# of the html file and the values the new one:
+# 
+# redirectSameRelease = {
+#     'X.Y': {
+#         '/old/path.html': '/new/path.html',
+#         ...
+#     },
+#     ...
+# }
+#
 
 redirectSameRelease = {
     '4.4': {
---
+++ source/_variables/settings.py
@@ -0,0 +1,25 @@
+###############################################################################
+#
+# Release settings
+#
+# This file contains the information the release settings that is unique to 
+# this branch:
+#
+# * is_latest_release: this variable must be set to False except for the
+#     latest release, where it must set to True.
+# * version: short X.Y version
+# * release: long X.Y.z version
+# * apiURL: URL of the Wazuh API reference spec in the Wazih repository (public)
+# * api_tag: use this variable to change the branch containing the specs that
+#     has to be shown.
+#
+
+# The short X.Y version
+version = '4.4'
+is_latest_release = False
+
+# The full version, including alpha/beta/rc tags
+# Important: use a valid branch (4.0) or, preferably, tag name (v4.0.0)
+release = '4.4.5'
+api_tag = 'v4.4.5'
+apiURL = 'https://raw.githubusercontent.com/wazuh/wazuh/'+api_tag+'/api/api/spec/spec.yaml'
