9a943a157cb66af3574f3cfb47b0acedf46e6ab3
Sandra Ocando <sandra.ocando@wazuh.com>
2023-09-06 09:15:59 +0200

    Update API tag

--- source/_variables/settings.py
+++ source/_variables/settings.py
@@ -21,6 +21,6 @@ is_latest_release = True
 # The full version, including alpha/beta/rc tags
 # Important: use a valid branch (4.0) or, preferably, tag name (v4.0.0)
 release = '4.5.2'
-api_tag = '4.5.2'
+api_tag = 'v4.5.2'
 
 apiURL = 'https://raw.githubusercontent.com/wazuh/wazuh/'+api_tag+'/api/api/spec/spec.yaml'
