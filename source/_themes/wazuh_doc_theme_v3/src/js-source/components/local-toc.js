b739ee26009fb92fa03420d86e3a7cc9e708bba7
vamera <vanesa.mejias@wazuh.com>
2023-09-11 12:13:55 +0200

    Fixed first link of the local-toc when using docutils>=0.17

--- source/_themes/wazuh_doc_theme_v3/src/js-source/components/local-toc.js
+++ source/_themes/wazuh_doc_theme_v3/src/js-source/components/local-toc.js
@@ -5,12 +5,24 @@
  */
 
 /* Fix for the first item when using the Bootstrap scrollspy in our documentation */
-if ( $('#local-toc').length > 0 ) {
-  /* Fix the href of the first item */
-  const firstLocalTocItem = $('#local-toc > .navbar-nav > .nav-item:first-of-type > .nav-link');
-  const firstSection = $('main:first-of-type > .section:first-of-type');
-  firstLocalTocItem.attr('href', firstLocalTocItem.attr('href') + firstSection.attr('id'));
-}
+$(document).ready(function() {
+  if ($('#local-toc').length > 0) {
+     /* Fix the href of the first item */
+    const firstLocalTocItem = $('#local-toc > .navbar-nav > .nav-item:first-of-type > .nav-link');
+    let firstSection = $('main:first-of-type > .section:first-of-type');
+
+    /* Fix for docutils>=0.17 */ 
+    if (firstSection.length === 0) {
+      firstSection = $('main:first-of-type > section:first-of-type');
+    }
+
+    // Check if the firstSection has a valid id before updating the href
+    const firstSectionId = firstSection.attr('id');
+    if (firstSectionId) {
+      firstLocalTocItem.attr('href', firstLocalTocItem.attr('href') + firstSectionId);
+    }
+  }
+});
 
 /* Expand accordion functionality for the local-toc */
 $('#local-toc .nav-link').on('click', function(e) {
