131afd3458c8a69949ae8e40ee673c2e92589c65
Gastón Palomeque <gaston.palomeque@wazuh.com>
2024-03-18 11:49:11 -0300

    Bump python minimum version

--- source/_variables/replacements.py
+++ source/_variables/replacements.py
@@ -39,7 +39,7 @@ custom_replacements = {
     "|WAZUH_CURRENT_MAJOR|" : "4.x",
     "|WAZUH_CURRENT_MINOR|" : version,
     "|WAZUH_CURRENT|" : release,
-    "|PYTHON_CLOUD_CONTAINERS_MIN|": "3.7",
+    "|PYTHON_CLOUD_CONTAINERS_MIN|": "3.8",
     "|PYTHON_CLOUD_CONTAINERS_MAX|": "3.11",
 
     # --- Revision numbers for Wazuh agent and manager packages versions
