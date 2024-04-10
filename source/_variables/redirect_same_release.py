50143f620c969b87e1b98ee048ab826040b247ed
Javier Medeot <javier.medeot@wazuh.com>
2024-04-10 16:49:14 -0300

    Simplify example

--- source/_variables/redirect_same_release.py
+++ source/_variables/redirect_same_release.py
@@ -13,10 +13,6 @@
 #   '4.2': {
 #     '/release-notes/release_4_2_0.html':
 #       '/release-notes/release-4-2-0.html',
-#     '/user-manual/registering/agent-enrollment.html':  
-#       '/user-manual/agent-enrollment/via-agent-configuration/index.html',
-#     '/azure/dependencies.html':  
-#       '/azure/index.html',
 #   }
 # }
 
