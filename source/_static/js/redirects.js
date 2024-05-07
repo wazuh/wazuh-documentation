2c93d2e979fb73c418b14f9e25ec500a7150dfc5
Daniel Sappa <93778492+tdrauncieal@users.noreply.github.com>
2024-05-07 17:12:04 -0300

    Update source/_static/js/redirects.js
    Co-authored-by: Javier Medeot <javier.medeot@wazuh.com>

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -87,7 +87,7 @@ newUrls['4.9'] = [
   '/deployment-options/offline-installation/index.html',
   '/deployment-options/offline-installation/step-by-step.html',
   '/deployment-options/offline-installation/installation-assistant.html',
-  '/deployment-options/offline-installation/coredump.html',
+  '/development/coredump.html',
 ];
 
 /* Pages no longer available in 4.9 */
