0680cf0f3b85a72c938bb7077972597ae68c90e2
Nicolas Agustin Guevara Pihen <42900763+Tostti@users.noreply.github.com>
2024-05-21 08:32:57 -0300

    Update source/_static/js/redirects.js
    Co-authored-by: Javier Medeot <javier.medeot@wazuh.com>

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -106,7 +106,7 @@ redirections.push(
 /* Redirections from 4.7 to 4.8  */
 
   {
-    'target': ['4.7=>4.8'],
+    'target': ['4.7=>4.8', '4.8 => 4.7'],
     '4.7': '/user-manual/wazuh-dashboard/settings.html',
     '4.8': '/user-manual/wazuh-dashboard/management.html',
   },
