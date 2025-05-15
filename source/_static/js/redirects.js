b5a5aa91768798df0bff0b6fe0697c45edb8d914
Ayooluwa Akindeko <akindekoayooluwa@gmail.com>
2025-05-15 12:22:24 +0200

    extra 5 renaming to 6

--- source/_static/js/redirects.js
+++ source/_static/js/redirects.js
@@ -5,7 +5,6 @@ const redirections = [];
 /* Note: new release versions must always be inserted in the first position of the array "versions" */
 const versions = [
   '6.0',
-  '5.0',
   '4.13',
   '4.12',
   '4.11',
